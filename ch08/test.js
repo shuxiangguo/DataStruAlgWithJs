a0797e486c6fdb2c592e6bfcced0a56fc2e653c1
shuxiangguo <shuxiangguo7@gmail.com>
2018-03-06 19:14:31 +0800

    add some file

--- ch08/test.js
+++ ch08/test.js
@@ -2,9 +2,30 @@
 function HashTable() {
     this.table  = new Array(137);
     this.simpleHash = simpleHash;
+    this.betterHash = betterHash;
     this.showDistro = showDistro;
     this.put = put;
-    // this.get = get;
+    this.buildChain = buildChain;
+    //this.get = get;
+}
+
+function buildChain() {
+    for (var i = 0; i < this.table.length; i++) {
+        this.table[i] = new Array();
+    }
+}
+function betterHash(string) {
+    const H = 37;
+    var total = 0;
+    for (var i = 0; i < string.length; i++) {
+        total += H*total + string.charCodeAt(i);
+    }
+    total = total % this.table.length;
+    if (total < 0) {
+        total += this.table.length -1;
+    }
+    console.log(string + "->" + parseInt(total));
+    return parseInt(total);
 }
 
 function simpleHash(data) {
@@ -12,25 +33,26 @@ function simpleHash(data) {
     for (var i = 0; i < data.length; i++) {
         total += data.charCodeAt(i);
     }
-    return total / this.table.length;
+    return total % this.table.length;
 }
 
 function put(data) {
-    var pos = simpleHash(data);
+    var pos = this.betterHash(data);
     this.table[pos] = data;
 }
 
 function showDistro() {
-    var n = 0;
     for (var i = 0; i < this.table.length; i++) {
-        if (this.table[i] !== undefined) {
+        if (this.table[i][0] !== undefined) {
             console.log(i + ": " + this.table[i]);
         }
     }
 }
 
-var someNames = ["David", "Jennifer", "Donnie", "Cynthia", "Mike", "Clayton", "Dannay", "Jonathan"];
+var someNames = ["David", "Jennifer", "Donnie", "Raymond",
+    "Cynthia", "Mike", "Clayton", "Danny", "Jonathan"];
 var hTable = new HashTable();
+hTable.buildChain();
 for (var i = 0; i < someNames.length; i++) {
     hTable.put(someNames[i]);
 }
